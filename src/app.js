2f1c1fcac0c12154d9225826d4618f4ebee7ec33
Saraaltaweel <s.bata97.st@gmail.com>
2021-08-18 00:25:56 +0300

    lab32

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 import ToDo from './components/todo/todo.js';
-
+import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+// import ToDo from './components/todo/todo';
+import SettingsForm from './components/settingForm';
+import ListProvider from './context/ListContext';
+import SettingsProvider from './context/SettingContext';
 export default class App extends React.Component {
   render() {
     return (
-      <ToDo />
+      <>
+      <Router>
+          <Switch>
+            <SettingsProvider>
+              <ListProvider>
+                <Route exact path='/'>
+                  <ToDo />
+                </Route>
+                <Route exact path='/settings'>
+                  <SettingsForm/>
+                </Route>
+              </ListProvider>
+            </SettingsProvider>
+          </Switch>
+        </Router>
+    </>
     );
   }
 }
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,59 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState ,useContext} from 'react';
 import useForm from '../../hooks/form.js';
 import List from '../list';
 import FormInfo from '../formInfo';
-
+import { ListContext } from '../../context/ListContext.js';
+import SettingsContext from '../../context/SettingContext.js';
 import { v4 as uuid } from 'uuid';
 
-const ToDo = () => {
+function ToDo (props) {
 
-  const [list, setList] = useState([]);
-  const [incomplete, setIncomplete] = useState([]);
-  const { handleChange, handleSubmit } = useForm(addItem);
+  const listContext=useContext(ListContext)
+  const [state, setState] = useState([]);
 
-  function addItem(item) {
-    console.log(item);
-    item.id = uuid();
-    item.complete = false;
-    setList([...list, item]);
-  }
+  // const [list, setList] = useState([]);
+  // const [incomplete, setIncomplete] = useState([]);
+  // const { handleChange, handleSubmit } = useForm(addItem);
+
 
   function deleteItem(id) {
     const items = list.filter( item => item.id !== id );
     setList(items);
   }
 
-  function toggleComplete(id) {
-
-    const items = list.map( item => {
-      if ( item.id == id ) {
-        item.complete = ! item.complete;
-      }
-      return item;
-    });
-
-    setList(items);
-
-  }
-
-  useEffect(() => {
-    let incompleteCount = list.filter(item => !item.complete).length;
-    setIncomplete(incompleteCount);
-    document.title = `To Do List: ${incomplete}`;
-  }, [list]);
+  useEffect(()=>{
+    let localSetting=localStorage.getItem('settings')===true;
+    if(localSetting){
+      localSetting=JSON.parse(localStorage.getItem('settings'))
+      SettingsContext.setSettings(localSetting.items,localSetting.view)
+    }
+ 
+  },[])
 
   return (
     <>
       <header>
-        <h1>To Do List: {incomplete} items pending</h1>
+        <h2>
+          There are {listContext.list.filter(item => !item.complete).length} Items To Complete
+        </h2>
       </header>
-
-     <FormInfo handleChange={handleChange} handleSubmit={handleSubmit}/>
-      <List toggleComplete={toggleComplete} list={list}/>
-
-
+        <div>
+          <FormInfo/>
+        </div>
+        <div>
+          <List list={state}/>
+        </div>
     </>
   );
-};
-
+}
 export default ToDo;
