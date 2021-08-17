522da27aab05cc09fcd47d3b68b591a5957c1fdc
Saraaltaweel <s.bata97.st@gmail.com>
2021-08-17 16:10:22 +0300

    lab31

---
+++ src/components/todo/todo.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import useForm from '../../hooks/form.js';
+import List from '../list';
+import FormInfo from '../formInfo';
+
+import { v4 as uuid } from 'uuid';
+
+const ToDo = () => {
+
+  const [list, setList] = useState([]);
+  const [incomplete, setIncomplete] = useState([]);
+  const { handleChange, handleSubmit } = useForm(addItem);
+
+  function addItem(item) {
+    console.log(item);
+    item.id = uuid();
+    item.complete = false;
+    setList([...list, item]);
+  }
+
+  function deleteItem(id) {
+    const items = list.filter( item => item.id !== id );
+    setList(items);
+  }
+
+  function toggleComplete(id) {
+
+    const items = list.map( item => {
+      if ( item.id == id ) {
+        item.complete = ! item.complete;
+      }
+      return item;
+    });
+
+    setList(items);
+
+  }
+
+  useEffect(() => {
+    let incompleteCount = list.filter(item => !item.complete).length;
+    setIncomplete(incompleteCount);
+    document.title = `To Do List: ${incomplete}`;
+  }, [list]);
+
+  return (
+    <>
+      <header>
+        <h1>To Do List: {incomplete} items pending</h1>
+      </header>
+
+     <FormInfo handleChange={handleChange} handleSubmit={handleSubmit}/>
+      <List toggleComplete={toggleComplete} list={list}/>
+
+
+    </>
+  );
+};
+
+export default ToDo;
